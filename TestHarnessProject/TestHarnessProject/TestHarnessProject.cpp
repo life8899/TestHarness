3794faf123ee645b5e5fe0f36c239e50f4852a1d
Kuohsun Tsai <kuohsunt@gmail.com>
2019-04-13 19:22:26 -0500

    d2ae8925d9139bbdfebe4c8ab0ee4df3f05c246f

--- TestHarnessProject/TestHarnessProject/TestHarnessProject.cpp
+++ TestHarnessProject/TestHarnessProject/TestHarnessProject.cpp
@@ -8,8 +8,11 @@ const double PI = 3.1415926;
 int main()
 {
 	double radius;
-	double height;
+	
+	double cylinder_radius;
+	double cylinder_height;
 	double X, Y;
+	string color;
 
 	circleType circle1;
 	cylinderType cylinder1;
@@ -17,18 +20,24 @@ int main()
 
 	cout << fixed << showpoint << setprecision(2);
 
+	cout << "Enter radius of the circle : " << endl;
+	cin >> radius;
+	circle1.setRadius(radius);
+
 	cout << "Enter cylinder height and radius : " << endl;
-	cin >> height >> radius;
+	cin >> cylinder_height >> cylinder_radius;
 
-	cylinder1.setRadius(radius);
-
-	cylinder1.setHeight(height);
+	cylinder1.setRadius(cylinder_radius);
+	cylinder1.setHeight(cylinder_height);
 
 	cout << "Enter two number of the center of a cylinder : " << endl;
 	cin >> X >> Y;
 
+	cout << "========================== " <<endl;
+	circle1.print();
+	cout << endl;
+	cout << "========================== " << endl;
 	cylinder1.setCenter(X, Y);
-
 	cylinder1.print();
 
 
